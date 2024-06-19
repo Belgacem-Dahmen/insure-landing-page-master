9de0070c1dc9247e6b67c398d6cfc305fcd00f17
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-06-19 12:33:04 +0200

    adding JS actions to mobile Menu

---
+++ index.js
@@ -0,0 +1,21 @@
+// buttons
+const menu__btn = document.getElementById('menu__btn')
+const close__btn = document.getElementById('close__btn')
+
+
+// divs
+const mobile__menu = document.getElementById('mobile__menu')
+
+
+menu__btn.addEventListener('click', () => {
+    mobile__menu.classList.remove('hidden')
+    menu__btn.classList.add('hidden')
+    close__btn.classList.remove('hidden')
+})
+
+
+close__btn.addEventListener('click',() => {
+    mobile__menu.classList.add('hidden')
+    menu__btn.classList.remove('hidden')
+    close__btn.classList.add('hidden')
+})
